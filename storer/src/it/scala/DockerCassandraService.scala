86e4db7435d0ec9aaa1dbb5b058a37fd788ad8e0
Juan Méndez <juan.m@47deg.com>
2018-03-28 04:15:57 +0200

    increase timeout in container creation to allow cassandra to start (gossip to settle)

--- storer/src/it/scala/DockerCassandraService.scala
+++ storer/src/it/scala/DockerCassandraService.scala
@@ -1,15 +1,18 @@
 package storer
 
 import com.whisk.docker.{DockerContainer, DockerKit, DockerReadyChecker}
+import scala.concurrent.duration._
 
 trait DockerCassandraService extends DockerKit {
+
+  override val StartContainersTimeout = 40.seconds
 
   val cassandraContainer: DockerContainer =
     DockerContainer(image = "cassandra:3.11.2")
       .withPorts(9042 -> Some(InitializeTests.cassandraHostPort))
       .withReadyChecker(DockerReadyChecker.LogLineContains("Starting listening for CQL clients on"))
 
-    abstract override def dockerContainers: List[DockerContainer] =
+  abstract override def dockerContainers: List[DockerContainer] =
       cassandraContainer :: super.dockerContainers
 
 }
